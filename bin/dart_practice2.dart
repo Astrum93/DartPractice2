4fc374bd6311f84f614ea73ebd96d587075bd18b
Astrum93 <jungmin2670@gmail.com>
2023-10-26 23:55:15 +0900

    231026 강의 예제 예외처리 부분

--- bin/dart_practice2.dart
+++ bin/dart_practice2.dart
@@ -139,15 +139,62 @@ void main() {
   ///   선행 진행 / 반복 될 코드
   /// } while (조건);
 
-  int num = 0;
+  // int num = 0;
 
-  do {
-    num++;
+  // do {
+  //   num++;
 
-    if (num == 4) {
-      continue;
-    }
+  //   if (num == 4) {
+  //     continue;
+  //   }
 
-    print('Running Do While $num');
-  } while (num < 10);
+  //   print('Running Do While $num');
+  // } while (num < 10);
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+  /// 예외처리 : 프로그램이 진행 중일 때, 의도하였거나 / 의도하지 않은 상황에 대해서
+  /// 프로그램적으로 오류가 발생했을 때, 대처하는 방법.
+  /// try - catch문 : 가장 기본적인 예외처리문 / 많이 쓰이는 예외처리 문이기도 함.
+  /// finaly문
+  /// on문
+  /// throw / rethrow문
+
+  // try {
+  //   /// 예외가 발생할 것으로 예상되는 코드
+  //   print(10 ~/ 0);
+  // } catch (error, stack) {
+  //   /// 예외가 발생했을 때 실행 하고자 하는 코드
+  //   print(error);
+  //   print(stack);
+  // } finally {
+  //   /// 예외가 발생 했던, 하지 않았던, try 문 / catch 문 이후에 실행하고자 하는 코드
+  //   print('예외처리 문을 지나쳤습니다.');
+  // }
+  // print('위의 에러 때문에 동작을 하지 않습니다.');
+
+  // try {
+  //   /// 예외가 발생할 것으로 예상되는 코드
+  //   print(10 ~/ 0);
+  // } on UnsupportedError catch (e) {
+  //   print('~/ 해당 연산자는 0으로 나눌 수 없습니다.');
+  // } on TypeError catch (e) {
+  //   print('Null 값이 참조 되었습니다.');
+  // } catch (e) {
+  //   print('모르는 에러가 발생했습니다.');
+  // }
+  // print('위의 에러 때문에 동작을 하지 않습니다.');
+
+  try {
+    /// 예외가 발생할 것으로 예상되는 코드
+    throw Exception('Unkonwn Error');
+  } on UnsupportedError {
+    print('~/ 해당 연산자는 0으로 나눌 수 없습니다.');
+  } on TypeError {
+    print('Null 값이 참조 되었습니다.');
+  } catch (e) {
+    /// main() 밖으로 던져버리는 코드.
+    rethrow;
+  }
+  print('위의 에러 때문에 동작을 하지 않습니다.');
 }
