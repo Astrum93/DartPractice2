83f6f43d4f06537b8b8f72b27f593a4a9d3a4616
Astrum93 <jungmin2670@gmail.com>
2023-10-26 23:08:15 +0900

    first commit

---
+++ bin/dart_practice2.dart
@@ -0,0 +1,26 @@
+void main() {
+  /// Dart 기초 예제 연습2
+  /// 분기문과 반복문
+  ///
+  /// 분기문 : 프로그램이 특정한 상황에 일을 할 지 / 하지 않고 넘길지
+  /// if / else || switch
+
+  /// if문 / if - else 문
+  /// if (조건식) {
+  ///   조건식이 참일 경우 실행 할 코드
+  /// } else {
+  ///   조건식이 거짓일 경우 실행 할 코드
+  /// }
+  ///
+
+  bool isTrue = true;
+  String text = 'Hello';
+
+  if (isTrue) {
+    print('True');
+  }
+
+  if (text == 'Hello') {
+    print('True');
+  }
+}
